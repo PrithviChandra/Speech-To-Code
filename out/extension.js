b912d3adb65f8e262acd5d1f701de912b81ecb65
Thomas George <thomas2000george@gmail.com>
2022-04-20 12:46:11 +0530

    Added extension

--- out/extension.js
+++ out/extension.js
@@ -2,21 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
-require('dotenv').config();
+require("dotenv").config();
 function activate(context) {
     const { Configuration, OpenAIApi } = require("openai");
     const configuration = new Configuration({
-        apiKey: "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",
+        apiKey: "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",
     });
     const openai = new OpenAIApi(configuration);
-    let disposable = vscode.commands.registerCommand('speechtocode.extensionMaker', async () => {
+    let code_gen = vscode.commands.registerCommand("speechtocode.extensionMaker", async () => {
         //read line from cursor
         const activeEditor = vscode.window.activeTextEditor;
         if (activeEditor != null) {
             let lineNumber = activeEditor.selection.active.line;
             let line = activeEditor.document.lineAt(lineNumber).text;
             //display line to user
-            let comment = (" \"\"\" " + line + " \"\"\" ");
+            let comment = ' """ ' + line + ' """ ';
             const response = await openai.createCompletion("code-davinci-001", {
                 prompt: comment,
                 temperature: 0,
@@ -27,14 +27,122 @@ function activate(context) {
             });
             //make an api call to openai with the line
             if (activeEditor) {
-                activeEditor.edit(editBuilder => {
-                    editBuilder.replace(activeEditor.selection.start, "#" + comment);
+                activeEditor.edit((editBuilder) => {
                     editBuilder.insert(activeEditor.selection.active, "\n" + response.data.choices[0].text + "\n");
                 });
             }
         }
     });
-    context.subscriptions.push(disposable);
+    //To calculate time complexity of generated code
+    let complexity = vscode.commands.registerCommand("speechtocode.time_complexity", async () => {
+        //read line from selection
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor != null) {
+            let line = activeEditor.selection;
+            let text = activeEditor.document.getText(line);
+            const response = await openai.createCompletion("text-davinci-002", {
+                prompt: text + "\nThe time complexity of this function is",
+                temperature: 0,
+                max_tokens: 100,
+                top_p: 1.0,
+                frequency_penalty: 0.0,
+                presence_penalty: 0.0,
+                stop: ["\n"],
+            });
+            if (activeEditor) {
+                activeEditor.edit((editBuilder) => {
+                    editBuilder.insert(activeEditor.selection.active, "\n" +
+                        " The time complexity is " +
+                        response.data.choices[0].text +
+                        "\n");
+                });
+            }
+        }
+    });
+    //Python Bug Fixer
+    let bug_fixer = vscode.commands.registerCommand("speechtocode.bug_fixer", async () => {
+        //read selected code
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor != null) {
+            let line = activeEditor.selection;
+            let text = activeEditor.document.getText(line);
+            //api call
+            const response = await openai.createCompletion("code-davinci-002", {
+                prompt: "##### Fix bugs in the below function\n \n### Buggy Python\n\n" +
+                    text +
+                    "\n ### Fixed Python",
+                temperature: 0,
+                max_tokens: 182,
+                top_p: 1.0,
+                frequency_penalty: 0.0,
+                presence_penalty: 0.0,
+                stop: ["###"],
+            });
+            //displaying response
+            if (activeEditor) {
+                activeEditor.edit((editBuilder) => {
+                    editBuilder.insert(activeEditor.selection.active, "\n" + response.data.choices[0].text);
+                });
+            }
+        }
+    });
+    //Explain Code
+    let code_explanation = vscode.commands.registerCommand("speechtocode.explain_code", async () => {
+        //read selected code
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor != null) {
+            let line = activeEditor.selection;
+            let text = activeEditor.document.getText(line);
+            //api call
+            const response = await openai.createCompletion("code-davinci-002", {
+                prompt: text + "\n\n\"\"\"\nHere's what the above class is doing:\n",
+                temperature: 0,
+                max_tokens: 64,
+                top_p: 1.0,
+                frequency_penalty: 0.0,
+                presence_penalty: 0.0,
+                stop: ["\"\"\""],
+            });
+            //displaying response
+            if (activeEditor) {
+                activeEditor.edit((editBuilder) => {
+                    editBuilder.insert(activeEditor.selection.active, "\n" + response.data.choices[0].text);
+                });
+            }
+        }
+    });
+    // Translate from Python to other languages
+    let translated = vscode.commands.registerCommand("speechtocode.translate_code", async () => {
+        //read selected code
+        const activeEditor = vscode.window.activeTextEditor;
+        if (activeEditor != null) {
+            let line = activeEditor.selection;
+            let text = activeEditor.document.getText(line);
+            //api call
+            const response = await openai.createCompletion("code-davinci-002", {
+                prompt: "##### Translate this function  from Python into Haskell\n### Python\n" +
+                    text +
+                    "\n ### Haskell",
+                temperature: 0,
+                max_tokens: 54,
+                top_p: 1.0,
+                frequency_penalty: 0.0,
+                presence_penalty: 0.0,
+                stop: ["###"],
+            });
+            //displaying response
+            if (activeEditor) {
+                activeEditor.edit((editBuilder) => {
+                    editBuilder.insert(activeEditor.selection.active, "\n" + response.data.choices[0].text);
+                });
+            }
+        }
+    });
+    context.subscriptions.push(code_gen);
+    context.subscriptions.push(complexity);
+    context.subscriptions.push(bug_fixer);
+    context.subscriptions.push(code_explanation);
+    context.subscriptions.push(translated);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
